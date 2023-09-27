e869fafb3258a43aa0ea4fd6456bfdd36d064e32
Benjamin Donoff <benjamin@donoff.com>
2023-09-27 17:04:34 +0100

    Task Completed

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,12 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    for item in items:
+        if frequencies.get(str(item)) == None:
+            frequencies[str(item)] = 1
+        else:
+            frequencies[str(item)] = frequencies.get(str(item)) + 1
     return frequencies
+
+
+print(frequencies([100, 'Hello', '100', '100', 100]))
